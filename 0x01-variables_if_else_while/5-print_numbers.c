3be89aa3e9d8fb5d7cf4321d6435639f48285091
Geoffrey-69 <kinuthiageoffrey70@gmail.com>
2023-03-20 12:41:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar((a % 10) + '0');
+	putcher9'\n'0;
+	return (0);
+}
