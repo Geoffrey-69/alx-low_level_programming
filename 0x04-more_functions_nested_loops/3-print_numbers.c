d172c4eeb799bcd21ea7d267049d0753c9390331
Geoffrey-69 <kinuthiageoffrey70@gmail.com>
2023-03-23 19:05:26 -0700

    alx-low_level_programming

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,11 +9,12 @@
 
 void print_numbers(void)
 {
-	int num = 0;
+        int num = 0;
 
-	do {
-		_putchar(num * 48);
-		num++;
-	} while (num >= 0 && num <= 9);
-	_putchar('\n');
+        while (num <= 9)
+        {
+                _putchar(num * 48 + 48);
+                num++;
+        }
+        _putchar('\n');
 }
