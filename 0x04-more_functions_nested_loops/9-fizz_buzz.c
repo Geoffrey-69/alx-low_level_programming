fb030ae6edbec31d6cb44ae52c39942a32a48399
Geoffrey-69 <kinuthiageoffrey70@gmail.com>
2023-03-23 19:59:31 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,36 @@
  */
 int main(void)
 {
-	int num;
+        int num;
 
-	for (num = 1; num <= 100; num++)
-	{
-		if (num % 3 == 0 && !(num % 5 == 0))
-			printf("fizz");
-		else if (num % 5 == 0 && num % 5 == 0)
-			printf("buzz");
-		else if (num % 3 == 0 && num % 5 == 0)
-			printf("fizzbuzz");
-		else
-			printf("%d", num);
+        for (num = 1; num <= 100; num++)
+        {
+                if (num % 3 == 0 && !(num % 5 == 0))
+                        putchar('f');
+                        putchar('i');
+                        putchar('z');
+                        putchar('z');
+                else if (num % 5 == 0 && num % 5 == 0)
+                        putchar('b');
+                        putchar('u');
+                        putchar('z');
+                        putchar('z');
+                else if (num % 3 == 0 && num % 5 == 0)
+                        putchar('f');
+                        putchar('i');
+                        putchar('z');
+                        putchar('z');
+                        putchar('b');
+                        putchar('u');
+                        putchar('z');
+                        putchar('z');
+                else
+	printf("%d", num);
 
-		if (num != 100)
-			printf(" ");
-		else
-			printf("\n");
-	}
+	if (num != 100)
+	putchar(' ')
+	else
+	putchar('\n');
+        }
 	return (0);
 }
